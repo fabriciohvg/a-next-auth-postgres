62b84809fa798e77e18c0bdb66b4f63869d3fe75
Fabrício Guimarães <fabriciohenrique.eng@gmail.com>
2025-09-25 17:00:51 -0300

    tentativa de auth com template prisma ##ERROR##

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,9 +1,60 @@
+import CredentialsProvider from "next-auth/providers/credentials";
 import NextAuth from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
-import { prisma } from "@/lib/prisma";
-import GitHub from "next-auth/providers/github";
+import prisma from "@/lib/prisma";
+import bcrypt from "bcryptjs";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: PrismaAdapter(prisma),
-  providers: [GitHub],
+  providers: [
+    CredentialsProvider({
+      name: "credentials",
+      credentials: {
+        email: { label: "Email", type: "email" },
+        name: { label: "Name", type: "name" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password) {
+          throw new Error("Invalid credentials");
+        }
+
+        const user = await prisma.user.findUnique({
+          where: { email: credentials.email },
+        });
+
+        if (!user) {
+          return await prisma.user.create({
+            data: {
+              name: credentials.name ?? credentials.email,
+              email: credentials.email,
+              password: await bcrypt.hash(credentials.password, 10),
+            },
+          });
+        }
+
+        const isCorrectPassword = await bcrypt.compare(
+          credentials.password,
+          user.password
+        );
+
+        if (!isCorrectPassword) {
+          throw new Error("Invalid credentials");
+        }
+
+        return user;
+      },
+    }),
+  ],
+  pages: {
+    signIn: "/login",
+  },
+  callbacks: {
+    async jwt({ token, user }) {
+      return { ...token, id: token.id ?? user?.id };
+    },
+    async session({ session, token }) {
+      return { ...session, user: { ...session.user, id: token.id } };
+    },
+  },
 });
