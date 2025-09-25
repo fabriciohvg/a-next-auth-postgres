ce48a485af08529b622155fe4b3aa0a115d30951
Fabrício Guimarães <fabriciohenrique.eng@gmail.com>
2025-09-25 11:04:03 -0300

    prisma postinstall + header nav component add + improvments + dynamic_force for db

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,11 +1,9 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "./../../generated/prisma";
 
-const globalForPrisma = globalThis;
+const globalForPrisma = global || {};
 
-export const prisma =
-  globalForPrisma.prisma ??
-  new PrismaClient({
-    log: ["query"],
-  });
+const prisma = globalForPrisma.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
+export default prisma;
